1224089c8943264b793dcfb8236fdaacccf89a58
김종진 <martia0118@gmail.com>
2021-01-27 02:16:27 +0900

    입력 실패 시 안내 메세지 보강

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -54,10 +54,15 @@ public class MainDrive {
 					break;
 				}
 
+				else if(!isRangeOk){
+					//범위 검사에 실패를 했다면
+					System.out.println("1 ~ 45의 숫자로만 입력가능합니다.");
+				}
+				
 				else {
-					// 검사에 실패한 경우
-					// 안내메세지도 보여주자
-					System.out.println("잘못된 숫자 입니다. 다시 입력해 주세요");
+					//범위 검사는 맞았다면
+					System.out.println("이미 입력된 번호입니다");
+					System.out.println("다른 숫자로 입력을 해주세요");
 				}
 
 			}
